62dafbafa41d81370c2523d8ee2706f61c60d427
renanbronchart <renan.bronchart@gmail.com>
2018-04-09 23:40:45 +0200

    [Feat:init] First commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <header class="header">
+    <h1 class="header__title">I tested It</h1>
+    <TheNav></TheNav>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import TheNav from '@/components/TheNav.vue'
+
+export default Vue.extend({
+  name: 'TheHeader',
+  props: {
+    msg: {
+      type: String,
+      default: ''
+    }
+  },
+  components: {
+    TheNav
+  }
+})
+
+</script>
+
+<style lang='scss'>
+  .header {
+    background: green;
+    padding: 40px;
+  }
+
+  .header__title {
+    color: white;
+    position: relative;
+    &:after {
+      content: '';
+      width: 120%;
+      height: 2px;
+      position: absolute;
+      top: 100%;
+      left: 0;
+      background: white;
+    }
+  }
+</style>
---
+++ src/views/TheTest.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>The test</h1>
+  </div>
+</template>
