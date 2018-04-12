4edf520a0a805d2eace6a2d555ab001a653dbf43
renanbronchart <renan.bronchart@gmail.com>
2018-04-12 11:56:16 +0200

    [Feat:graphQl] Install vue-apollo and apollo-client

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,38 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import 'tachyons'
+
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+
 Vue.config.productionTip = false
+
+const httpLink = new HttpLink({
+  // You should use an absolute URL here
+  uri: 'https://api.graph.cool/simple/v1/cjfvm8as87e1i0120oii0tcbv'
+})
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+  defaultOptions: {
+    $loadingKey: 'loading'
+  }
+})
 
 new Vue({
   router,
   store,
+  provide: apolloProvider.provide(),
   render: h => h(App)
 }).$mount('#app')
