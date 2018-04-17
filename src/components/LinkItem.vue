8b6258c5fa13700b067afdf7a51b289773c5e200
renanbronchart <renan.bronchart@gmail.com>
2018-04-18 00:23:26 +0200

    [Feat:votes] Add schema votes

--- src/components/LinkItem.vue
+++ src/components/LinkItem.vue
@@ -1,17 +1,78 @@
 <template>
-  <div>{{ link.description }} ({{ link.url }})</div>
+  <div class="flex mt2 items-start">
+    <div class="flex items-center">
+      <span class="gray">{{ linkNumber }}.</span>
+      <div
+        v-if="userId"
+        class="ml1 gray f11 upvote"
+        @click="voteForLink()">
+        ▲
+      </div>
+    </div>
+    <div class="ml1">
+      <a
+        :href="link.url"
+        class="link"
+        target="_blank">
+        {{ link.description }} ({{ link.url }})
+      </a>
+      <div class="f6 lh-copy gray">
+        {{ link.votes.length }} votes | by {{ link.postedBy ? link.postedBy.name : 'Unknown' }} {{ timeDifferenceForDate(link.createdAt) }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+import timeDifferenceForDate from '@/utils/timeDifference'
+
+import { CREATE_VOTE_MUTATION } from '@/constants/graphql'
+import { GC_USER_ID } from '@/constants/settings'
+
 export default Vue.extend({
   name: 'LinkItem',
   props: {
-    'link': {
+    link: {
       default: null,
       type: Object
+    },
+    index: {
+      default: 0,
+      type: Number
+    }
+  },
+  computed: {
+    userId (): number {
+      return parseInt(this.$root.$data.userId)
+    },
+    linkNumber (): number {
+      let result = this.index + 1
+
+      if (this.$route.path.includes('new')) {
+        result = (this.pageNumber - 1) * this.linksPerPage + (this.index + 1)
+      }
+
+      return result
+    }
+  },
+  methods: {
+    timeDifferenceForDate,
+    voteForLink () {
+
     }
   }
 })
 </script>
+
+<style lang="scss" scoped>
+  .upvote {
+    cursor: pointer;
+  }
+
+  .link {
+    text-decoration: none;
+    color: black;
+  }
+</style>
--- src/constants/graphql.ts
+++ src/constants/graphql.ts
@@ -8,6 +8,16 @@ export const ALL_LINKS_QUERY = gql`
       createdAt
       url
       description
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
     }
   }
 `
@@ -31,15 +41,37 @@ export const CREATE_LINK_MUTATION = gql`
   }
 `
 
+export const CREATE_VOTE_MUTATION = gql`
+  mutation CreateVoteMutation($userId: ID!, $linkId: ID!) {
+    createVote (
+      userId: $userId,
+      linkId: $linkId
+    ) {
+      id,
+      user {
+        id
+      }
+      link {
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
+
 export const CREATE_USER_MUTATION = gql`
   mutation CreateUserMutation($name: String!, $email: String!, $password: String!) {
     createUser(
       name: $name,
-      authProvider: { 
-        email: { 
-          email: $email, 
-          password: $password 
-        } 
+      authProvider: {
+        email: {
+          email: $email,
+          password: $password
+        }
       }
     ) {
       id
